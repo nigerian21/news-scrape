dc6466b9b5062521a349952c3ef6460535e4db1f
Efe <efeowolide@gmail.com>
2019-03-25 19:05:24 -0500

    Having trouble getting route working

--- server.js
+++ server.js
@@ -17,12 +17,6 @@ app.set("view engine", "handlebars");
 var db = require("./models");
 
 var PORT = 1515;
-
-
-// Configure middleware
-
-
-// Parse request body as JSON
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // Make public a static folder
@@ -34,6 +28,7 @@ mongoose.connect("mongodb://localhost/newsScraper", { useNewUrlParser: true });
 // Routes
 
 // A GET route for scraping the chicago tribune
+//not able to get route working
 app.get("/scrape", function(req, res) {
   // First, we grab the body of the html with axios
   axios.get("https://www.chicagotribune.com/news/local/breaking/").then(function(response) {
@@ -100,15 +95,10 @@ app.get("/articles/:id", function(req, res) {
     });
 });
 
-// Route for saving/updating an Article's associated Note
 app.post("/articles/:id", function(req, res) {
   // Create a new note and pass the req.body to the entry
   db.Note.create(req.body)
     .then(function(dbNote) {
-      // If a Note was created successfully, find one Article with an `_id` equal to `req.params.id`. Update the Article to be associated with the new Note
-      // { new: true } tells the query that we want it to return the updated User -- it returns the original by default
-      // Since our mongoose query returns a promise, we can chain another `.then` which receives the result of the query
-      return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
     })
     .then(function(dbArticle) {
       // If we were able to successfully update an Article, send it back to the client
